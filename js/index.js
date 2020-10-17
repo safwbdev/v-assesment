78b4491552e62c2aaa777d0f0ed7ac493124732e
Stefaan <safwebdev@gmail.com>
2020-10-17 17:38:55 +0800

    fixes

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2.2,
+          slidesToShow: 2,
           slidesToScroll: 1,
           arrows: false,
         },
